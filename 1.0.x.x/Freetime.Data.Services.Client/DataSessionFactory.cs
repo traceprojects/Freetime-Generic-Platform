9cb02cf15f08061d8071a4b03898b53084cde9b7
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-10-05 13:36:32 +0800

    Added Data Services DataSessionFactory

---
+++ 1.0.x.x/Freetime.Data.Services.Client/DataSessionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Freetime.Base.Business;
+using Freetime.Base.Business.Implementable;
+
+namespace Freetime.Data.Services.Client
+{
+    public class DataSessionFactory : IDataSessionFactory
+    {
+        TContract IDataSessionFactory.GetDataSession<TContract>(ILogic logic, TContract defaultContract)
+        {
+            return defaultContract;
+        }
+    }
+}
