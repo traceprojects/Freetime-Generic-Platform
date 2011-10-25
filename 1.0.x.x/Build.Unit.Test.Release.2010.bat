8361e2fd9ca8bbd1a622d5b43d7803d9a94b6d5b
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-10-25 17:35:00 +0800

    Added Build Script from Mike / Updated 3.5 projects to 4.0

---
+++ 1.0.x.x/Build.Unit.Test.Release.2010.bat
@@ -0,0 +1,11 @@
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+
+echo Building 900.Freetime.Unit.Test.2010.sln
+echo.
+devenv /build release "1Solution.2010\900.Freetime.Unit.Test.2010.sln"
+echo. 
+echo Done Building 900.Freetime.Unit.Test.2010.sln
+echo.
+
+pause
