67681fdad0c0ba6a3ef78c525e58bddf344f373d
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-11-02 12:59:11 +0800

    added LanguageList

---
+++ 1.0.x.x/Freetime.Base.Data/Collection/LanguageList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using Freetime.Base.Data.Entities;
+
+namespace Freetime.Base.Data.Collection
+{
+    [Serializable]
+    [CollectionDataContract]
+    [XmlRoot("Languages",
+        Namespace = "http://www.freetime-generic.com",
+        IsNullable = true)]
+    public class LanguageList : List<Language>
+    {
+    }
+}
