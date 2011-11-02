2bd62a75f8f378195f8b538ca0c88b3f74b7bb59
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-11-02 11:05:26 +0800

    added eventlog

---
+++ 1.0.x.x/Freetime.Base.Framework/Diagnostics/LogUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Freetime.Base.Framework.Diagnostics
+{
+    public class LogUtil
+    {
+    }
+}
