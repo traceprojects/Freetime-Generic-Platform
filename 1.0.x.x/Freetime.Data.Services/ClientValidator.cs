0be22a85187b74367fb18933bf4398820ec720fb
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-11-02 13:32:27 +0800

    added client validator

---
+++ 1.0.x.x/Freetime.Data.Services/ClientValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Freetime.Data.Services
+{
+    public class ClientValidator : System.IdentityModel.Selectors.UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName == null)
+                throw new ArgumentNullException("userName");
+
+            if (password == null)
+                throw new ArgumentNullException("password");
+
+            throw new Exception("Hello World");
+        }
+    }
+}
