62d449630625f37d73436b6c3574c27cf1edbb63
Michael Dela Cuesta <michael.dcuesta@gmail.com>
2011-11-15 11:14:00 +0800

    Added BusinessLogicBuilder Config

---
+++ 1.0.x.x/Freetime.Base.Component/Configuration/BusinessLogicBuilderConfigurationAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+
+
+namespace Freetime.Base.Component.Configuration
+{
+    public class BusinessLogicBuilderConfigurationAttribute : ConfigurationElement
+    {
+        [ConfigurationProperty("Key", IsRequired = true)]
+        public string Key
+        {
+            get
+            {
+                return (string)this["Key"];
+            }
+            set
+            {
+                this["Key"] = value;
+            }
+        }
+
+        [ConfigurationProperty("Value", IsRequired = true)]
+        public string Value
+        {
+            get
+            {
+                return (string)this["Value"];
+            }
+            set
+            {
+                this["Value"] = value;
+            }
+        }
+    }
+}
